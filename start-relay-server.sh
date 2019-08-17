9c745d5cee405e34f35cd2733937ea3aef80a200
Ariel Mendelzon <amendelzon@gmail.com>
2019-08-17 12:57:01 +1200

    Removing unused scripts

--- start-relay-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./build/server/bin/RelayHttpServer --Url http://172.17.0.2:2222 --Port 2222 --RelayHubAddress 0x7557fcE0BbFAe81a9508FF469D481f2c72a8B5f3 --EthereumNodeUrl http://172.17.0.1:4444 --Workdir ./build/server/
