68767e4023eac51b14ef4f6c183ff36d34d223b5
Ariel Mendelzon <amendelzon@gmail.com>
2019-09-19 12:01:05 +1200

    Several UI fixes to the demo given testnet usage findings

--- src/js/demo/app/app.js
+++ src/js/demo/app/app.js
@@ -6,6 +6,7 @@ const { rpcHost, contractAddress } = require('./config.json');
 
 let counter;
 let wallet;
+let incrementing = false;
 
 const initializeWallet = function() {
     const store = {
@@ -15,44 +16,64 @@ const initializeWallet = function() {
     wallet = new Wallet(store);
 }
 
-const refresh = (account, row) => async () => {
-    counter.setAccount(account);
-    const count = await counter.getCount();
-    $('.counter-row-count .count', row).text(count);
-};
-
-const increment = (account, row) => async () => {
-    counter.setAccount(account);
+const disable = (row) => {
     $('.counter-row-refresh', row).attr('disabled', true);
     $('.counter-row-increment', row).attr('disabled', true);
     $('.counter-row-count .count', row).css('display', 'none');
     $('.counter-row-count .loading', row).css('display', '');
+};
 
-    try {
-        await counter.increment();
-    } catch (e) {
-        console.log('ERROR TRYING TO INCREMENT', e);
-    }
-    await refresh(account, row)();
-
+const enable = (row) => {
     $('.counter-row-count .count', row).css('display', '');
     $('.counter-row-count .loading', row).css('display', 'none');
     $('.counter-row-refresh', row).attr('disabled', false);
-    $('.counter-row-increment', row).attr('disabled', false);
+    $('.counter-row-increment', row).attr('disabled', incrementing);
 };
 
-const renderAccount = async (account) => {
+const setIncrementsEnabled = (enabled) => {
+    $('.counter-row-increment').attr('disabled', !enabled);
+};
+
+const refresh = async (account, row, quiet) => {
+    !quiet && disable(row);
+
+    const count = await counter.getCount(account);
+    $('.counter-row-count .count', row).text(count);
+
+    !quiet && enable(row);
+};
+
+const increment = async (account, row) => {
+    disable(row);
+    setIncrementsEnabled(false);
+
+    incrementing = true;
+    try {
+        await counter.increment(account);
+    } catch (e) {
+        console.log('ERROR TRYING TO INCREMENT', e);
+    }
+    await refresh(account, row, true);
+    incrementing = false;
+
+    enable(row);
+    setIncrementsEnabled(true);
+};
+
+const renderAccount = (account) => {
     const newRow = $('.counter-row-template').clone();
     newRow.removeClass('counter-row-template').addClass('counter-row');
     newRow.css('display', '');
     $('.counter-row-address', newRow).text(account.address);
-    await refresh(account, newRow)();
-    $('.counter-row-refresh', newRow).click(refresh(account, newRow));
-    $('.counter-row-increment', newRow).click(increment(account, newRow));
+    $('.counter-row-refresh', newRow).click(() => refresh(account, newRow));
+    $('.counter-row-increment', newRow).click(() => increment(account, newRow));
     $('.counters-body').append(newRow);
+    // This is asynchronous, but we don't want to wait, otherwise we'll stall
+    // other potentially rendering accounts
+    refresh(account, newRow);
 };
 
-const newAccount = async () => {
+const newAccount = () => {
     renderAccount(wallet.newAccount());
 };
 
--- src/js/demo/app/counter.js
+++ src/js/demo/app/counter.js
@@ -13,39 +13,23 @@ const Counter = function(url, address) {
     this.contract.setProvider(this.client.currentProvider);
 };
 
-Counter.prototype.setAccount = function(account) {
-    this.account = account;
-    this.client.currentProvider.relayClient.useKeypairForSigning(account);
-};
-
-Counter.prototype.getAccount = function() {
-    return this.account;
-};
-
 Counter.prototype.ensureContractInstance = async function() {
     if (this.contractInstance == null) {
         this.contractInstance = await this.contract.at(this.address);
     }
 };
 
-Counter.prototype.ensureAccount = function() {
-    if (this.account == null) {
-        throw "Account not set";
-    }
+Counter.prototype.getCount = async function(account) {
+    await this.ensureContractInstance();
+    return await this.contractInstance.get({ from: account.address });
 };
 
-Counter.prototype.getCount = async function() {
-    this.ensureAccount();
+Counter.prototype.increment = async function(account) {
     await this.ensureContractInstance();
-    return await this.contractInstance.get({ from: this.account.address });
-};
-
-Counter.prototype.increment = async function() {
-    this.ensureAccount();
-    await this.ensureContractInstance();
+    this.client.currentProvider.relayClient.useKeypairForSigning(account);
     // gasPrice in null will make the RelayClient choose a gasPrice dependant on
     // the current network gas price
-    return await this.contractInstance.increment({ from: this.account.address, gasPrice: null });
+    return await this.contractInstance.increment({ from: account.address, gasPrice: null });
 };
 
 module.exports = {
