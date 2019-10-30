bcdf6e80af3313737dc5fcc0d7283cfcced2c006
Ariel Mendelzon <amendelzon@rsk.co>
2019-10-30 13:36:56 +1300

    Fixing staking and funding values in contract and relay funding scripts

--- scripts/fundcontract.js
+++ scripts/fundcontract.js
@@ -2,6 +2,8 @@
 const Web3 = require('web3')
 const irelayhub = require( '../src/js/relayclient/IRelayHub')
 
+const FUND_AMOUNT = 0.04;
+
 async function fundcontract(hubaddr, contaddr, fromaddr, fund, web3) {
     let rhub = new web3.eth.Contract(irelayhub, hubaddr)
     let balance = await rhub.methods.balanceOf(contaddr).call();
@@ -26,7 +28,7 @@ async function run() {
 
     if (!hubaddr) {
         console.log("usage: fundcontract.js {hubaddr} {contaddr} {from-account} {nodeurl}")
-        console.log("fund amount is fixed on 1 eth")
+        console.log(`fund amount is fixed on ${FUND_AMOUNT} eth`)
         console.log("node url defaults to 'http://localhost:8545'")
         process.exit(1)
     }
@@ -34,7 +36,7 @@ async function run() {
     const web3 = new Web3(new Web3.providers.HttpProvider(ethNodeUrl))
 
     let accounts = await web3.eth.getAccounts()
-    fundcontract(hubaddr, contaddr, accounts[fromaccount], 1.1e18, web3)
+    fundcontract(hubaddr, contaddr, accounts[fromaccount], FUND_AMOUNT*1e18, web3)
 }
 
 run()
